ccc59d11ad57bd41f08a9304ff9dd4b120e8e6d9
lenovo <mukeshsharma31994@gmail.com>
2022-09-22 23:12:45 +0100

    123

--- src/main/java/com/dgsl/imp/entity/Constant.java
+++ src/main/java/com/dgsl/imp/entity/Constant.java
@@ -138,7 +138,7 @@ public class Constant {
 	public static TestEntity getBodyEnity() {
 		TestEntity lEntity = new TestEntity();
 
-		lEntity.setDepartment("IMPP");
+		lEntity.setDepartment("IPP");
 		lEntity.setDepositRate("123518");
 		lEntity.setEmpID("145258");
 		lEntity.setLink("www.google.com");
